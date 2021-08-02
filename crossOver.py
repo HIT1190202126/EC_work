b2a3d8cf30edfb51da44eb71c6132f521cd3786f
HITlyz2001 <hitlyz2001@163.com>
2021-08-02 20:42:35 +0800

    V0.2

---
+++ crossOver.py
@@ -0,0 +1,5 @@
+
+class crossOver:
+    def cycle_crossover(self, parent1, parent2):
+
+        return  child1,child2
---
+++ mutation.py
@@ -0,0 +1,8 @@
+
+
+class Mutation:
+    def swapMutation(self,ind):
+        #######
+        return ind
+    def insertMutation(self,ind):
+        pass
